2f5ce12ff6471613977b6445dfef37a0b4431e63
Matteo Leturcq--Daligaux <matteo.leturcq--daligaux@etu.minesparis.psl.eu>
2023-03-30 23:13:34 +0200

    CSV of matrix

--- interface.py
+++ interface.py
@@ -4,15 +4,18 @@ from tkinter import filedialog
 
 import pandas as pd
 import matplotlib
+
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from tkinter import ttk
 import matplotlib.pyplot as plt
 
+
 def quit_me():
     root.quit()
     root.destroy()
+
 
 root = tk.Tk()
 root.protocol("WM_DELETE_WINDOW", quit_me)
@@ -22,13 +25,14 @@ root.geometry("700x400")
 label = tk.Label(root, text="Choose your file")
 label.pack()
 
+
 def plot_csv2(path):
-    data = pd.read_csv(path, index_col="Bal",delimiter=";")
+    data = pd.read_csv(path, index_col="Bal", delimiter=";")
     columns = data.columns
     data_to_plot = columns[2]
-    
+
     val = str_to_float_v(data[data_to_plot].values)
-    figure = Figure(figsize=(5,5), dpi=100)
+    figure = Figure(figsize=(5, 5), dpi=100)
     p1 = figure.add_subplot(111)
 
     p1.plot(val)
@@ -39,13 +43,14 @@ def plot_csv2(path):
     plt.grid()
 
     try:
-        plt.savefig('Plot/'+new_name(path[5:],'png'))
+        plt.savefig("Plot/" + new_name(path[5:], "png"))
     except FileNotFoundError:
         os.mkdir("Plot")
-        plt.savefig('Plot/'+new_name(path[5:],'png'))
-    
+        plt.savefig("Plot/" + new_name(path[5:], "png"))
+
     canvas = FigureCanvasTkAgg(figure, root)
     canvas.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
+
 
 def select_file():
     file_path = filedialog.askopenfilename(initialdir="Data")
@@ -53,7 +58,8 @@ def select_file():
     file_path_fin = "Data/" + L[-1]
     plot_csv2(file_path_fin)
 
-button = tk.Button(root, text="Submit", command=select_file)
+
+button = ttk.Button(root, text="Submit", command=select_file)
 button.pack()
 
 root.mainloop()
